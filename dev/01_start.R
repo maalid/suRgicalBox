716f23c71dd61c9c90a973d96375ddbf10a4eb5e
maalid <maalid@gmail.com>
2020-11-26 11:43:42 -0300

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,66 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "suRgicalBox", # The Name of the package containing the App 
+  pkg_title = "Web-app para asistir el armado de cajas quirurgicas", # The Title of the package containing the App 
+  pkg_description = "Applicación web que asiste, agiliza y facilita el armado de cajas quirurgicas, 
+                     reduciendo a cero los errores de identificacion de instrumentos mediante el uso
+                     de redes neuronales profundas. Integra en el flujo de trabajo la captura y 
+                     preprocesamiento de imágenes, el entrenamiento de redes neuronales siamesas,
+                     la inferencia de resultados y la generación de reportes para hacer gestión.
+                     Su uso está orientado para el armado de cajas quirurgicas, sin embargo, el proceso
+                     es generico, por lo que puede extenderse su uso a otras aplicaciones sin problemas.", # The Description of the package containing the App 
+  author_first_name = "Marcelo", # Your First Name
+  author_last_name = "Alid-Vaccarezza", # Your Last Name
+  author_email = "maalidvacc@outlook.com", # Your Email
+  repo_url = "https://github.com/maalid/suRgicalBox.git" # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( name = "Marcelo Alid-Vaccarezza" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::remove_favicon()
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
